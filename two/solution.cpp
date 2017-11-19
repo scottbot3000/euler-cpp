8cf06fe78db37b7864f0d251450bd1964620c87d
scottbot3000 <scottbot3000@users.noreply.github.com>
2017-11-19 14:25:46 -0800

    correct solution, not finalized

---
+++ two/solution.cpp
@@ -0,0 +1,11 @@
+int solution () {
+	int maxTerm = 4000000;
+	int t1 = 1, t2 = 2;
+	int sum = 0;
+	while (t2 < maxTerm) {
+		if (t2 % 2 == 0) sum += t2;
+		swap(t1, t2);
+		t2 = t1 + t2;
+	}
+	return sum;
+}
